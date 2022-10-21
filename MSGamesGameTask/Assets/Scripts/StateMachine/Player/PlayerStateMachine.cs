2bd1f67d32cfd581b82822d6f486258f59f2d5df
Adrian Komuda <adrian.komuda.dev@gmail.com>
2022-10-21 16:32:38 +0200

    Animations connected to the player.

--- MSGamesGameTask/Assets/Scripts/StateMachine/Player/PlayerStateMachine.cs
+++ MSGamesGameTask/Assets/Scripts/StateMachine/Player/PlayerStateMachine.cs
@@ -1,17 +1,23 @@
 using Core;
 using Locomotion.Player;
+using UnityEngine;
 
 namespace StateMachine.Player
 {
     public class PlayerStateMachine : StateMachine
     {
+        [field: SerializeField] public float AnimationCrossFadeDuration { get; private set; } = 0.1f;
+        [field: SerializeField] public float AnimatorDampTime { get; private set; } = 0.05f;
+        
         public InputReader InputReader { get; private set; }
         public PlayerMover PlayerMover { get; private set; }
+        public Animator Animator { get; private set; }
 
         private void Awake()
         {
             InputReader = GetComponent<InputReader>();
             PlayerMover = GetComponent<PlayerMover>();
+            Animator = GetComponent<Animator>();
         }
 
         private void Start()
