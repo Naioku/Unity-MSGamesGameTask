066c22551f87a7ede4263d1508d38880a789f290
Adrian Komuda <adrian.komuda.dev@gmail.com>
2022-10-24 11:58:52 +0200

    Set up idling.

---
+++ MSGamesGameTask/Assets/Scripts/StateMachine/AI/AIPatrollingState.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace StateMachine.AI
+{
+    public class AIPatrollingState : AIBaseState
+    {
+        private static readonly int LocomotionStateHash = Animator.StringToHash("Locomotion");
+        private static readonly int MovementSpeedHash = Animator.StringToHash("MovementSpeed");
+        
+        public AIPatrollingState(AIStateMachine stateMachine) : base(stateMachine) {}
+        
+        public override void Enter()
+        {
+            StateMachine.Animator.CrossFadeInFixedTime(LocomotionStateHash, StateMachine.AnimationCrossFadeDuration);
+        }
+
+        public override void Tick()
+        {
+            
+        }
+
+        public override void Exit()
+        {
+        }
+    }
+}
--- MSGamesGameTask/Assets/Scripts/StateMachine/AI/AIStateMachine.cs
+++ MSGamesGameTask/Assets/Scripts/StateMachine/AI/AIStateMachine.cs
@@ -1,7 +1,22 @@
-﻿namespace StateMachine.AI
+﻿using UnityEngine;
+
+namespace StateMachine.AI
 {
     public class AIStateMachine : StateMachine
     {
+        [field: SerializeField] public float AnimationCrossFadeDuration { get; private set; } = 0.1f;
+        [field: SerializeField] public float AnimatorDampTime { get; private set; } = 0.05f;
         
+        public Animator Animator { get; private set; }
+
+        private void Awake()
+        {
+            Animator = GetComponent<Animator>();
+        }
+
+        private void Start()
+        {
+            SwitchState(new AIPatrollingState(this));
+        }
     }
 }
